906ae10b5753a73087743bfe52760c68b7dc4145
Christian <chrille_0313@hotmail.com>
2021-03-15 11:28:48 +0100

    Better CMD Visualization
    Ruby2d Not Implemented

--- draw_to_console.rb
+++ draw_to_console.rb
@@ -3,7 +3,6 @@ require 'fiddle/struct'
 require 'fiddle/cparser'
 require 'fiddle/import'
 
-
 def winsize
     #Ruby 1.9.3 added 'io/console' to the standard library.
     require 'io/console'
@@ -23,68 +22,9 @@ getstdhandle = Fiddle::Function.new(kernel32["GetStdHandle"], [Fiddle::TYPE_INT]
 
 $handle = getstdhandle.call(-11)
 
-
 module CONSOLEHELPER
     extend Fiddle::Importer
-    dlload(Dir.pwd + "/WRITECONSOLE.dll")
+    dlload("kernel32.dll")
 
-    extern 'void WriteToConsole(int, void*, int, int, int)'
-end
-
-
-# Draw visualization
-def visualize(numbers, sleep_time=1)
-    
-    # Make drawing-grid
-    console_rows = []
-
-    row = 0
-    while row < $rows
-        console_rows[row] = " " * $cols
-        
-        row += 1
-    end
-    
-    i = 0
-    while i < numbers.length
-        j = $rows - numbers[i]
-
-        while j < $rows - 1
-            #console_rows[j][i] = "█"
-            console_rows[j][i] = "X"
-            
-            j += 1
-        end
-
-        i += 1
-    end
-
-
-    # Make borders around drawing area
-    for border in 0..$rows - 1 do
-        console_rows[border][0] = "|"
-        console_rows[border][$cols - 1] = "|"
-    end
-
-    for border in 0..$cols - 1 do
-        console_rows[0][border] = "-"
-        console_rows[$rows - 1][border] = "-"
-    end
-
-    console_rows[0][0] = "+"
-    console_rows[0][$cols-1] = "+"
-    console_rows[$rows-1][0] = "+"
-    console_rows[$rows-1][$cols-1] = "+"
-
-
-    # Draw rows
-    final_field = ""
-    for row in console_rows do
-        final_field += row
-    end
-    #system("cls")
-    CONSOLEHELPER.WriteToConsole($handle, final_field, final_field.length, 0,0)
-
-    #puts(final_field)
-    sleep(sleep_time/1000)
+    extern 'int WriteConsoleOutputCharacterA(int, void*, int, void*, void*)'
 end
--- visualizer.rb
+++ visualizer.rb
@@ -1,72 +1,124 @@
 require_relative 'draw_to_console.rb'
+require 'ruby2d'
 require 'win32/sound'
 include Win32
 
 
-# Bubblesort
-def bubblesort(arr)
-    arrLength = arr.length - 1
+# Ruby2d Visualization
 
-    for counter in 0..arrLength
-        for i in 0..(arrLength - counter - 1)
-            if arr[i] > arr[i + 1]
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                visualize(arr) #  Draw
-                #Sound.beep(arr[i] * 100, 150)
+set title: "Sorting Visualizer", width: 800, height: 800, diagnostics: true  # Window properties
+
+
+
+Triangle.new
+
+def visualize(numbers, current=0, checking=1, ticks=1)
+
+    barWidth = Window.width.to_f / numbers.length.to_i
+
+    update do
+        clear
+
+        numbers.each_with_index do |number, index|
+            currentRect = Rectangle.new(x: index * barWidth, y: Window.height - number, width: barWidth, height: number)
+        
+            if index == current
+                currentRect.color = 'green'
+            elsif index == checking
+                currentRect.color = 'red'
+            else
+                currentRect.color = 'white'
             end
         end
-    end
 
-    #for item in arr do
-    #    Sound.beep(item * 100, 10)
-    #end
+        arrLength = numbers.length - 1
 
-    #Sound.play("SystemAsterisk", Sound::ALIAS)
-end
+        for counter in 0..arrLength
+            for i in 0..(arrLength - counter - 1)
+                if numbers[i] > numbers[i + 1]
+                    numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
 
-
-# Quicksort
-def swap(arr, pos1, pos2)
-    arr[pos1], arr[pos2] = arr[pos2], arr[pos1]
-    visualize(arr) #  Draw
-end
-
-def partition(arr, low, high)
-    pivotValue = arr[high]
-    pivotIndex = low
-
-    for i in low..high do
-        if arr[i] < pivotValue
-            swap(arr, i, pivotIndex)
-            pivotIndex += 1
+                end
+            end
         end
+
     end
 
-    swap(arr, pivotIndex, high)
-    return pivotIndex
+    show
+
 end
-
-def quicksort(arr, low=0, high=-1)
-    if high == -1
-        high = arr.length - 1
-    end
-    
-    if low < high
-        index = partition(arr, low, high)
-        quicksort(arr, low, index - 1)
-        quicksort(arr, index + 1, high)
-    end
-end
-
-
 
 numbers = []
-for i in 1..$rows - 1 do
-    numbers.append(i)
+for i in 1..25 - 1 do
+    numbers.append(rand(Window.height))
 end
 
 numbers = numbers.shuffle
-bubblesort(numbers)
-sleep(3)
-numbers = numbers.shuffle
-quicksort(numbers)
+
+visualize(numbers)
+
+
+# Cmd visualization
+def visualize_cmd(numbers, sleep_time=1)
+    
+    # Make drawing-grid
+    console_rows = []
+
+    row = 0
+    while row < $rows
+        console_rows[row] = " " * $cols
+        
+        row += 1
+    end
+    
+    i = 0
+    while i < numbers.length
+        j = $rows - numbers[i]
+
+        while j < $rows - 1
+            #console_rows[j][i] = "█"
+            console_rows[j][i] = "X"
+            
+            j += 1
+        end
+
+        i += 1
+    end
+
+
+    # Make borders around drawing area
+    for border in 0..$rows - 1 do
+        console_rows[border][0] = "|"
+        console_rows[border][$cols - 1] = "|"
+    end
+
+    for border in 0..$cols - 1 do
+        console_rows[0][border] = "-"
+        console_rows[$rows - 1][border] = "-"
+    end
+
+    console_rows[0][0] = "+"
+    console_rows[0][$cols-1] = "+"
+    console_rows[$rows-1][0] = "+"
+    console_rows[$rows-1][$cols-1] = "+"
+
+
+    # Draw rows
+    final_field = ""
+    for row in console_rows do
+        final_field += row
+    end
+    #system("cls")
+
+    
+    free = Fiddle::Function.new(Fiddle::RUBY_FREE, [Fiddle::TYPE_VOIDP], Fiddle::TYPE_VOID)
+    p = Fiddle::Pointer.malloc(Fiddle::SIZEOF_INT, free)
+    
+    coord = Fiddle::Pointer.malloc(Fiddle::SIZEOF_INT, free)
+    coord = 0
+    
+    CONSOLEHELPER.WriteConsoleOutputCharacterA($handle, final_field, final_field.length, coord, p)
+
+    #puts(final_field)
+    sleep(sleep_time.to_f/1000.0)
+end
